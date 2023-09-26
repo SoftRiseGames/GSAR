3af4923d8c2a8fb34a11237b169cfbe3ff91885b
SoftRiseGames <98230446+SoftRiseGames@users.noreply.github.com>
2023-09-26 19:00:52 +0300

    last

--- Assets/Script/TargetRaycastSystem.cs
+++ Assets/Script/TargetRaycastSystem.cs
@@ -5,9 +5,20 @@ using UnityEngine;
 public class TargetRaycastSystem : MonoBehaviour
 {
     public LayerMask groundLayer;
+    public LineRenderer lineRenderer;
+    RaycastHit dedect;
+    public Transform pos1;
+    public Transform pos2;
+    private float counter;
+    public float dist;
+    [SerializeField] float lineDrawSpeed;
     // Start is called before the first frame update
     void Start()
     {
+        lineRenderer = pos1.gameObject.GetComponent<LineRenderer>();
+        lineRenderer.enabled = false;
+        counter = 0;
+        lineRenderer.SetPosition(0, pos1.position);
     }
 
     // Update is called once per frame
@@ -15,13 +26,51 @@ public class TargetRaycastSystem : MonoBehaviour
     {
 
         //Debug.DrawLine(transform.position, transform.position + transform.up * -50, Color.green);
-        RaycastHit dedect;
+        
         
         if(Physics.Raycast(this.transform.position,transform.TransformDirection(Vector3.up)*-1,out dedect,Mathf.Infinity,groundLayer))
         {
             Debug.Log(dedect.collider.name);
             Debug.DrawRay(transform.position,transform.TransformDirection(Vector3.up)*-1 * dedect.distance, Color.green);
+            
+            pos2 = dedect.transform;
+            dist = Vector3.Distance(pos1.position, pos2.position);
+            if (Input.GetMouseButton(0))
+            {
+                LineRendererSystem();
+            }
         }
-        
+        else
+        {
+            pos2 = null;
+            counter = 0;
+            dist = 0;
+            lineRenderer.enabled = false;
+        }
+
+        if(pos2 != null)
+            LineRendererAnimation();
+
+        Debug.Log(counter);
+
+    }
+    public void LineRendererSystem()
+    {
+        lineRenderer.enabled = true;
+    }
+    public void LineRendererAnimation()
+    {
+        if (counter < dist)
+        {
+           
+            counter += .1f / lineDrawSpeed;
+            float x = Mathf.Lerp(0, dist, counter);
+
+            Vector3 pointA = pos1.position;
+            Vector3 pointB = pos2.position;
+
+            Vector3 pointAlongLine = x * Vector3.Normalize(pointB - pointA) + pointA;
+            lineRenderer.SetPosition(1, pointAlongLine);
+        }
     }
 }
